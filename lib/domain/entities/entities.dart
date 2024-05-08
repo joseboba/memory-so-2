8539c417a202e206d00d57f91ecef6a8d71b5194
Jose Bobadilla <jbobadilla@is4tech.com>
2024-05-08 17:55:13 -0600

    CLOCK ALGORITHM

---
+++ lib/domain/entities/entities.dart
@@ -0,0 +1,1 @@
+export 'clock_item.dart';
--- lib/presentation/blocs/service_locator.dart
+++ lib/presentation/blocs/service_locator.dart
@@ -1,8 +1,10 @@
 import 'package:get_it/get_it.dart';
+import 'package:memory/presentation/blocs/blocs.dart';
 import 'package:memory/presentation/blocs/nru/nru_bloc.dart';
 
 GetIt getIt = GetIt.instance;
 
 void serviceLocatorInit() {
   getIt.registerSingleton(NruBloc());
+  getIt.registerSingleton(ClockBloc());
 }
